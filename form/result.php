09f34bf737ddc416c4c31df59c2d58c12d0f7ceb
Zac <a19860101@gmail.com>
2021-06-16 19:08:17 +0800

    20210616

--- form/result.php
+++ form/result.php
@@ -5,11 +5,22 @@
 
     // print_r($_GET);
 
-    if($_POST){
-        // print_r($_POST);
-        echo $_POST["name"];
-    }else{
-        // print_r($_GET);
-        echo $_GET["name"];
-        echo $_GET["mail"];
-    }
+    // if($_POST){
+    //     // print_r($_POST);
+    //     echo $_POST["name"];
+    // }else{
+    //     // print_r($_GET);
+    //     echo $_GET["name"];
+    //     echo $_GET["mail"];
+    // }
+
+    // print_r($_REQUEST);
+    // echo $_REQUEST["name"];
+    // echo $_REQUEST["mail"];
+
+    // $name = $_REQUEST["name"];
+    // $mail = $_REQUEST["mail"];
+
+    extract($_REQUEST);
+    echo $name;
+    echo $mail;

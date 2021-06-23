3d2d983bf073902a14e5998f21f4c007937fd425
Zac <a19860101@gmail.com>
2021-06-23 21:20:15 +0800

    20210623

--- students/db.php
+++ students/db.php
@@ -1,9 +1,29 @@
 <?php
+
+    // 方法一
+    // $db_host = "localhost";
+    // $db_user = "admin";
+    // $db_pw = "admin";
+    // $db_name = "php-13-20210607";
+
+    // $db = mysqli_connect($db_host,$db_user,$db_pw,$db_name)or die("Database Error!!");
+
+    // mysqli_query($db , "SET NAMES utf8mb4");
+
+    // 方法二
     $db_host = "localhost";
     $db_user = "admin";
     $db_pw = "admin";
     $db_name = "php-13-20210607";
+    
+    //建立mysqli物件實體
+    $db = new mysqli($db_host,$db_user,$db_pw,$db_name);
 
-    $db = mysqli_connect($db_host,$db_user,$db_pw,$db_name)or die("Database Error!!");
+    print_r($db);
 
-    mysqli_query($db , "SET NAMES utf8mb4");
+    if($db->connect_errno){
+        echo $db->connect_error;
+        return ;
+    }
+
+    $db->query("SET NAMES utf8mb4");

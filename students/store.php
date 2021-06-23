3cce30e0e1484ef9de2585d37161041b121f789c
Zac <a19860101@gmail.com>
2021-06-23 19:35:00 +0800

    20210623

--- students/store.php
+++ students/store.php
@@ -6,4 +6,11 @@
     $sql = "INSERT INTO students(name,mail,gender,edu,skills,content)
             VALUES('$name','$mail','$gender','$edu','$skills','$content')";
     mysqli_query($db,$sql);
+
+    // header("location:index.php");
+    // header("refresh:3;url=index.php");
+    
+    
+    echo "<script>alert('資料已新增!');</script>";
+    header("refresh:0;url=index.php");
     

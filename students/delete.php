0e051f98db284a1da1ddb1e210b705d1d6c8b5fe
Zac <a19860101@gmail.com>
2021-06-28 19:05:56 +0800

    20210628

--- students/delete.php
+++ students/delete.php
@@ -1,9 +1,20 @@
 <?php
+    //方法一
+    // require_once("db.php");
+    // extract($_REQUEST);
+    // // echo $id;
+    // // $id = $_REQUEST["id"];
+    // $sql = "DELETE FROM students WHERE id = {$id}";
+    // mysqli_query($db,$sql);
+    // echo "<script>alert('資料已刪除!');</script>";
+    // header("refresh:0;url=index.php");
+
+    //方法二
     require_once("db.php");
-    extract($_REQUEST);
-    // echo $id;
-    // $id = $_REQUEST["id"];
-    $sql = "DELETE FROM students WHERE id = {$id}";
-    mysqli_query($db,$sql);
+	extract($_REQUEST);
+    $sql = "DELETE FROM students WHERE id = ?";
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param("i",$id);
+    $stmt->execute();
     echo "<script>alert('資料已刪除!');</script>";
     header("refresh:0;url=index.php");

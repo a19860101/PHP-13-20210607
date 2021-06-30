11045e9da2acdddf1a14c91430e4c38bba07a0cd
Zac <a19860101@gmail.com>
2021-06-30 19:07:25 +0800

    20210630

--- students_pdo_function/function.php
+++ students_pdo_function/function.php
@@ -14,6 +14,15 @@
         $row = $stmt->fetch();
         return $row;
     }
+    function store($request){
+        extract($request);
+        $skills = implode(",",$skills);
+	    $sql = "INSERT INTO students(name,mail,gender,edu,skills,content)VALUES(?,?,?,?,?,?)";
+	    $stmt = pdo()->prepare($sql);
+	    $stmt->execute([$name,$mail,$gender,$edu,$skills,$content]);
+
+    }
+
     function edit(){
         extract($_REQUEST);
         $sql = "SELECT * FROM students WHERE id = ?";
@@ -21,4 +30,8 @@
         $stmt->execute([$id]);
         $row = $stmt->fetch();
         return $row;
+    }
+    function update(){}
+    function delete(){
+
     }
--- students_pdo_function/store.php
+++ students_pdo_function/store.php
@@ -1,10 +1,7 @@
 <?php
     require_once("pdo.php");
-	extract($_REQUEST);
-	$skills = implode(",",$skills);
-	$sql = "INSERT INTO students(name,mail,gender,edu,skills,content)VALUES(?,?,?,?,?,?)";
-	$stmt = $pdo->prepare($sql);
-	$stmt->execute([$name,$mail,$gender,$edu,$skills,$content]);
+	require_once("function.php");
+	store($_REQUEST);
 
 	echo "<script>alert('資料已新增!');</script>";
 	header("refresh:0;url=index.php");      

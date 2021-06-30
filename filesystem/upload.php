a0a2602c7798ecc6aec43d47606000b07b36857e
Zac <a19860101@gmail.com>
2021-06-30 20:13:54 +0800

    20210630

--- filesystem/upload.php
+++ filesystem/upload.php
@@ -18,11 +18,26 @@
     // echo "<br>";
     // echo $tmp_name;
 
+    $ext = pathinfo($name,PATHINFO_EXTENSION);
+    // $filename = pathinfo($name,PATHINFO_FILENAME);
+    // $basename = pathinfo($name,PATHINFO_BASENAME);
+    // $dirname = pathinfo($name,PATHINFO_DIRNAME);
+
+    // echo $ext;
+    // echo "<br>";
+    // echo $filename;
+    // echo "<br>";
+    // echo $basename;
+    // echo "<br>";
+    // echo $dirname;
+
+    $img = time().".".$ext;
+   
     if(!is_dir("images")){
         mkdir("images");
     }
 
-    $target = "images/{$name}";
+    $target = "images/{$img}";
     if($error == 0){
         if(move_uploaded_file($tmp_name,$target)){
             echo "上傳成功";

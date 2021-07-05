09e8a557f91627d8b9c0f1cc681d8796aaf60d9d
Zac <a19860101@gmail.com>
2021-07-05 21:12:21 +0800

    20210705

--- member/auth.php
+++ member/auth.php
@@ -9,6 +9,12 @@
 
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
+    if(!$row){
+        echo "<script>alert('帳號或密碼錯誤，請重新登入');</script>";
+        header("refresh:0;url=index.php");
+        return ;
+    }
+
     if($row["pw"] == $pw){
         $_SESSION["AUTH"] = $row;
         echo "<script>alert('歡迎登入');</script>";

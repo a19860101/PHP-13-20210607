0686e904421099e9d51eee4c666d476166394e0b
Zac <a19860101@gmail.com>
2021-07-05 20:34:33 +0800

    20210705

--- member/auth.php
+++ member/auth.php
@@ -2,11 +2,18 @@
     require_once("pdo.php");
     session_start();
    
-    extract($_POST);
+    extract($_REQUEST);
     $sql = "SELECT * FROM users WHERE user = ?";
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$user]);
 
-    $row = $stmt->fetch();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    print_r($row);
+    if($row["pw"] == $pw){
+        $_SESSION["AUTH"] = $row;
+        echo "<script>alert('歡迎登入');</script>";
+        header("refresh:0;url=index.php");
+    }else{
+        echo "<script>alert('帳號或密碼錯誤，請重新登入');</script>";
+        header("refresh:0;url=index.php");
+    }

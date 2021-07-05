55cbdbcad69c4788de2af126164b56b9dc6f69fb
Zac <a19860101@gmail.com>
2021-07-05 21:33:19 +0800

    20210705

--- member/store.php
+++ member/store.php
@@ -2,6 +2,18 @@
     require_once("pdo.php");
 
     extract($_REQUEST);
+
+    $sql_check = "SELECT * FROM users WHERE user = ?";
+    $stmt_check = $pdo->prepare($sql_check);
+    $stmt_check->execute([$user]);
+
+    if($stmt_check->rowCount() > 0){
+        echo "<script>alert('此帳號已被使用，請重新註冊');</script>";
+        header("refresh:0;url=register.php");
+        return;
+    }
+
+
     $sql = "INSERT INTO users(user,pw)VALUES(?,?)";
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$user,$pw]);

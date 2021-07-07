61613b48184719bd3db04f237cf0ffb504a41dd2
Zac <a19860101@gmail.com>
2021-07-07 20:22:24 +0800

    20210707

--- blog/post/function.php
+++ blog/post/function.php
@@ -1,8 +1,14 @@
 <?php
+    function now(){
+        date_default_timezone_set("Asia/Taipei");
+        $now = date("Y-m-d H:i:s");
+        return $now;
+    }
     function store($request){
         extract($request);
-	    $sql = "INSERT INTO posts(title,content,category_id,user_id,created_at,updated_at)VALUES(?,?,?,?,NOW(),NOW())";
+	    // $sql = "INSERT INTO posts(title,content,category_id,user_id,created_at,updated_at)VALUES(?,?,?,?,NOW(),NOW())";
+	    $sql = "INSERT INTO posts(title,content,category_id,user_id,created_at,updated_at)VALUES(?,?,?,?,?,?)";
 	    $stmt = pdo()->prepare($sql);
         $user_id = 1;
-	    $stmt->execute([$title,$content,$category_id,$user_id]);
+	    $stmt->execute([$title,$content,$category_id,$user_id,now(),now()]);
     }

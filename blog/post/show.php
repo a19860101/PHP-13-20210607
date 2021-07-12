d7f3a9da03ec818535b0bb67e2ce445a71b98f69
Zac <a19860101@gmail.com>
2021-07-12 19:06:42 +0800

    20210712

--- blog/post/show.php
+++ blog/post/show.php
@@ -16,6 +16,11 @@
                 最後更新時間 <?php echo $post["updated_at"]; ?>
             </div>
             <a href="<?php echo $webroot; ?>" class="btn btn-success">文章列表</a>
+            <a href="#" class="btn btn-info">編輯文章</a>
+            <form action="#" method="post" class="d-inline-block">
+                <input type="hidden" name="id" value="<?php echo $post["id"]; ?>">
+                <input type="submit" class="btn btn-danger" value="刪除文章" onclick="return confirm('確認刪除？')">
+            </form>
         </div>
     </div>
 </div>
